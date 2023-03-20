526121b1fe3b31cd33ba4c67ecc158535effd82e
justDanny <112081235+CallmeBabygirl@users.noreply.github.com>
2023-03-20 10:37:26 +0100

    .

--- PHKlassenBibliothek/PHMenue.cs
+++ PHKlassenBibliothek/PHMenue.cs
@@ -6,81 +6,10 @@ namespace PHKlassenBibliothek
     {
         public static void PH_Menue()
         {
-            bool Exit = false;
-
-            do
-            {
-
-                (int, int) cPosBM = Console.GetCursorPosition();
-
-                Console.WriteLine();
-
-                //ASCII art Logo wird erzeugt.
-                Console.WriteLine
-                    (FiggleFonts.Slant.Render("FOIMultiTool"));
-
-                //Konsolentitel wird geändert.
-                Console.Title = "FOIMultiTool";
-
-                Console.WriteLine("------------------------------------------------------------------------------------\n" +
-                                  "                              >>> Hauptmenü <<<\n" +
-                                  "------------------------------------------------------------------------------------\n\n");
-
-                Console.WriteLine("Eingabe: exit\t->\tbeendet das Programm");
-
-                //Beschreibung der Software.
-                Console.WriteLine("\n\nMit der Software 'FOIMulti-Tool' sollen wiederkehrende oder besonders aufwendige\n" +
-                                  "Aufgabenaus dem schulischen Kontext erleichtert oder gelöst werden. Diese Aufgaben\n" +
-                                  "ergeben sich aus den Problemstellungen aus dem Unterricht der Berufsfachschule für Technik.\n\n");
-
-                string HauptAusw;
-
-
-                //Eingabeaufforderung 
-                Console.WriteLine("Wählen Sie eine der folgenden Themenbereiche:\n");
-                Console.WriteLine("\te - Elektrotechnik");
-                Console.WriteLine("\ti - Informatik");
-                Console.WriteLine("\tm - Mathematik");
-                Console.WriteLine("\tp - Physik");
-                Console.WriteLine("\tw - Wirtschaft\n");
-                Console.Write("Eingabe:");
-                HauptAusw = Console.ReadLine().ToLower();
-
-                switch (HauptAusw)
-                {
-                    case "e":
-                        Console.Clear();
-                        //Hier das Etechnikmenü aufrufen
-                        break;
-
-                    case "i":
-                        Console.Clear();
-                        //Hier das Informationstechnikmenü aufrufen
-                        break;
-
-                    case "m":
-                        Console.Clear();
-                        //Hier das Mathematikmenü aufrufen
-                        break;
-
-                    case "p":
-                        Console.Clear();
-                        //Hier das Physikmenü aufrufen
                         bool subexit = false;
-                        bool neu = true;
                         do
                         {
-
                             Console.ForegroundColor = ConsoleColor.DarkYellow;
-                            Console.WriteLine("----------------------------------------------------------------------------------------------------------\n");
-                            Console.WriteLine("----             -----      ---      --     -------      ---          ----      ----      ----      ------\n");
-                            Console.WriteLine("----     -------   ----    -----    -----     ----     ----    ------------    ------    ----     --------\n");
-                            Console.WriteLine("----     -------   ----    -----    ------     --     ----     ------------    ------    --     ----------\n");
-                            Console.WriteLine("----     ------   -----    -----    --------       --------      ----------    ------         ------------\n");
-                            Console.WriteLine("----            -------             ---------     ------------      ------     ------         ------------\n");
-                            Console.WriteLine("----     --------------    -----    ---------     ---------------     ----     ------    --     ----------\n");
-                            Console.WriteLine("----     --------------    -----    ---------     -------------      -----     ------    ----     --------\n");
-                            Console.WriteLine("----     -------------      ---      --------     -------          ------      ----      ----      -------\n");
                             Console.WriteLine("----------------------------------------------------------------------------------------------------------\n");
 
                             Console.WriteLine("Willkommen im Themenbereich Physik :)\n" +
@@ -100,10 +29,6 @@ namespace PHKlassenBibliothek
 
                             Console.WriteLine("Eingabe: ");
                             string eingabe = Console.ReadLine();
-                            Console.Clear();
-                            do
-                            {
-
                                 if (eingabe == "1" | eingabe == "2" | eingabe == "3" | eingabe == "4")
                                 {
                                     int feature = int.Parse(eingabe);
@@ -125,7 +50,6 @@ namespace PHKlassenBibliothek
                                             //Feature 4 Simon simqn
                                             break;
                                     }
-
                                 }
                                 if (eingabe == "subexit")
                                 {
@@ -139,37 +63,13 @@ namespace PHKlassenBibliothek
                                 else
                                 {
                                     //Falsche Eingabe
-                                    Console.WriteLine("Das hat nicht geklappt...\nUm fortfahren zu können, geben Sie bitte eine der vier vorgegebenen Zahlen an oder 'subexit' um ins Hauptmenü zurück zu kehren.:)");
-                                    neu = false;
+                    Console.WriteLine("Das hat nicht geklappt...\n" +
+                        "Um fortfahren zu können, geben Sie bitte eine der vier vorgegebenen Zahlen an\n" +
+                        " oder 'subexit' um ins Hauptmenü zurückzukehren.:)");                                                                           
+                    Console.WriteLine("Um fortzufahren eine beliebige Taste drücken.");
                                     Console.ReadKey();
                                 }
-                            } while (!neu);
-
                         } while (!subexit);
-
-                        break;
-
-                    case "w":
-                        Console.Clear();
-                        //Hier das Wirtschaftsmenü aufrufen
-                        break;
-
-                    case "exit":
-                        Exit = true;
-                        break;
-                    default:
-
-                        Console.WriteLine("Ungültige Eingabe");
-                        Console.ReadKey();
-
-                        (int, int) cPosAM = Console.GetCursorPosition();
-
-                        KonsolenExtrasBibliothek.ConsoleExtras.ClearCurrentConsoleLine(cPosBM.Item2, cPosAM.Item2);
-
-                        break;
-                }
-
-            } while (!Exit);
         }
     }
 }
